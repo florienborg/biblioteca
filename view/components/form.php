6a1f96bc7cab6f620e3fdac58ca105d93b14e13b
genesis-nf <genefernandz@gmail.com>
2023-07-26 17:49:52 +0200

    Boton solucionado

--- view/components/form.php
+++ view/components/form.php
@@ -26,10 +26,10 @@
 
 
 
-<div class="container mt-5">
+<div class="container mt-5 mb-5">
   <div class="row fw-semibold" style="color:#581C87;">      
     <h2 class="text-center mb-5 mt-3">Introduce el libro nuevo</h2>
-    <div class="col-md-6 mb-4" >
+    <div class="col-md-6" >
       <form action="./view/logic/insertar.php" method="post">
         
         <div class="form-group">
@@ -39,12 +39,12 @@
 
         <div class="form-group">
              <label for="" class="mt-4" style="font-size:17px">Título del libro</label>
-             <input type="text" name="titulo" class="form-control mb-3 mt-2" style="background:#D1D1D1; height: 50px" placeholder="Título del libro">
+             <input type="text" name="titulo" class="form-control mb-3 mt-2" style="background:#D1D1D1; height: 50px" placeholder="">
         </div>
 
         <div class="form-group">
              <label for="" class="mt-4" style="font-size:17px">Autor</label>
-             <input type="text" name="autor" class="form-control mb-3 mt-2" style="background:#D1D1D1; height: 50px" placeholder="Autor del libro">
+             <input type="text" name="autor" class="form-control mt-2" style="background:#D1D1D1; height: 50px" placeholder="">
         </div>
         </div>
       
@@ -58,14 +58,13 @@
             <label for="" class="mt-4" style="font-size:17px">Subir portada</label>
             <input type="url" name="portada" id="portada" class="form-control mb-3 mt-2" style="border: 2px dashed #adb5bd; height: 110px; text-align: center" placeholder="Upload a file or drag and drop PNG, JPG, SVG up to 10MB."/>
         </div>
+        </div>
         
+        <div class="text-center">
+            <input type="submit" value="Guardar" class="btn w-25 mb-3 mt-5 .btn-custom:hover"  style="background:#C084FC; color: white; font-size:16px;">
+        </div>
       </form> 
       
     </div>
-    <div class="text-center">
-        <div>
-            <input type="submit" value="Guardar" class="btn w-25 mb-3 mt-3"  style="background:#C084FC; color: white; font-size:16px;">
-        </div
-    </div>
-  </div>
+</div>
 </div> 
