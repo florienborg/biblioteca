31e743b3551cf9d85470e0798dd200bcf6a15ac7
Cindy Leiva <cindyleiva376@gmail.com>
2023-07-27 10:14:22 +0200

    card

--- config/db.php
+++ config/db.php
@@ -1,9 +1,9 @@
 <?php
 function conectar(){
-    $server = "localhost:8889";
+    $server = "localhost";
     $user = "root";
-    $pass = "root";
-    // $pass = "";
+    // $pass = "root";
+    $pass = "";
     $db = "libreria";
     $conn = mysqli_connect($server, $user, $pass);
     mysqli_select_db($conn, $db); 
--- index.php
+++ index.php
@@ -17,21 +17,19 @@ $libros = mysqli_fetch_all($query, MYSQLI_ASSOC);
 </head>
 <body>
     <?php include("./view/components/navbar.php"); ?>
-        <div class="row mt-3">
-            <?php
+    <h1 class= "title" style="color: #581C87;padding: 4rem;text-align: center;">Descubre un mundo infinito entre las páginas: donde cada libro es una puerta hacia el conocimiento y la imaginación.</h1>
+    <div class="row mt-3" style=";align-items: center;display: flex; justify-content: center;" >
+        <?php
             foreach ($libros as $libro) {
                 ?>
                 <div class="col-md-3">
-                    <div class="card" style="width: 20rem; height: 35rem; margin: 10px;">
+                    <div class="card" style="width: 20rem; height: 32rem; margin: 2rem;">
                         <img src= "<?php echo $libro['portada']; ?>" class="card-img-top" alt="Portada del libro"  style="width: 100%; height: 18rem; object-fit: cover;">
                         <div class="card-body">
-                            <h2 class="card-title">Título: <?php echo $libro['titulo']; ?></h2>
-                            <h3 class="card-title">Autor: <?php echo $libro['autor']; ?></h3>
-                            <h5 class="card-title">ISBN: <?php echo $libro['ISBN']; ?></h5>
-                            <p class="card-text">Descripción: <?php echo $libro['descripcion']; ?></p>
-                            <a href="#" class="btn btn-primary">Información</a>
-                            <a href="./view/logic/actualizar.php?id=<?php echo $libro['ISBN']?>" class="btn btn-primary">Editar</a>
-                            <a href="./view/logic/delete.php?id=<?php echo $libro['ISBN']?>" class="btn btn-danger">Borrar</a>
+                            <a href="#" class="link-text" style="text-decoration:none;text-align:left;font-weight: 800;color: #581C87;"><h2 class="card-title"><?php echo $libro['titulo'];?></h2></a>
+                            <h3 class="card-title"style="text-align: left;font-weight: 400;"><?php echo $libro['autor']; ?></h3>
+                            <h5 class="card-title" style="text-align: left;font-weight: 300;">ISBN: <?php echo $libro['ISBN']; ?></h5>
+                            <a href="./view/logic/delete.php?id=<?php echo $libro['ISBN']?>" class="btn btn-danger" style="background: #C084FC; color: white; border: none; font-weight: 600;">Borrar</a>
                         </div>
                     </div>
                 </div>
